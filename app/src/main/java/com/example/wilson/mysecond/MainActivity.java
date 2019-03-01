be2058094574773d9c7bdcc747ad6136737457ba
wsnchristopher <wsnchristopher@gmail.com>
2019-03-01 11:02:15 +0100

    Initial commit

---
+++ app/src/main/java/com/example/wilson/mysecond/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.wilson.mysecond;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
